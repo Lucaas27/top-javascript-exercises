f0d00ef808ef0cbb1b2051e6008c5736f621e79b
Lucas <lucasggomees@gmail.com>
2022-06-02 17:33:23 +0100

    02 exercise completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
 
+    let resultStr = '';
+    if (num < 0) {
+        return 'ERROR';
+
+    } else {
+        for (let i = 0; i < num; i++) {
+            resultStr += string;
+        }
+
+        return resultStr;
+    }
 };
 
 // Do not edit below this line
